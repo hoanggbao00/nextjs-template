c9d8569f7f68ba62b393a27f4cbe5b1573cfa4b3
hoanggbao00 <kaitou.kurosaki@gmail.com>
2025-05-05 17:10:32 +0700

    chore: change lucide to tabler icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon } from "lucide-react";
+import { IconArrowRight } from "@tabler/icons-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -36,7 +36,7 @@ export default function Home() {
           >
             Read our docs
           </a>
-          <Button icon={ArrowRightIcon} iconPlacement="right" effect={"shine"}>
+          <Button icon={IconArrowRight} iconPlacement="right" effect={"shine"}>
             test
           </Button>
         </div>
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  experimental: {
+    reactCompiler: true,
+  },
+  distDir: process.env.NODE_ENV === "production" ? ".next" : ".next-dev",
 };
 
 export default nextConfig;
