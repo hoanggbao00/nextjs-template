9c2100ee61a2098d714e6b6db658d6930412a9fc
hoanggbao00 <kaitou.kurosaki@gmail.com>
2025-05-06 08:45:49 +0700

    refactor: update middleware to handle public routes for authentication redirects

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,29 @@
-// Match all paths except:
-// - api (API routes)
-// - _next/static (static files)
-// - _next/image (image optimization files)
-// - favicon.ico (favicon)
-
 import { type NextRequest, NextResponse } from "next/server";
 import { auth } from "./lib/auth";
+
+const publicRoutes = ["/sign-in"];
 
 export default async function middleware(req: NextRequest) {
   const session = await auth();
 
-  // If user is not authenticated and is not on the sign-in page, redirect to the sign-in page
-  if (!session && req.nextUrl.pathname !== "/sign-in") {
+  //* Redirect to sign-in page if user is not authenticated and is not on a public route
+  if (!session && !publicRoutes.includes(req.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
-  // If user is authenticated and is on the sign-in page, redirect to the home page
-  if (session && req.nextUrl.pathname === "/sign-in") {
+  //* Redirect to home page if user is authenticated and on a public route
+  if (session && publicRoutes.includes(req.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
 }
 
+// Match all paths except:
+// - api (API routes)
+// - _next/static (static files)
+// - _next/image (image optimization files)
+// - favicon.ico (favicon)
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
